93dd34655b0f7d776a7d3f74ea55b5b38b35cc5b
Roy Grizèll <rgrizell467@gmail.com>
2023-11-16 20:40:46 +0100

    register php
    added the php code to actually submit data to the sql database and added a page to know that the register suceded

---
+++ includes/register_page/fhl.inc.php
@@ -0,0 +1,30 @@
+<?php
+    require_once "dbh.inc.php";
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        $uname = $_POST["uname"];
+        $email = $_POST["email"];
+        $psw = $_POST["psw"];
+
+        try {
+
+            $qeury = "INSERT INTO users (username, email,psw) VALUES(:uname, :email,:psw);";
+
+            $stmt = $pdo->prepare($qeury);
+
+            // name all params
+            $stmt->bindParam("uname", $uname);
+            $stmt->bindParam("email", $email);
+            $stmt->bindParam("psw", $psw);
+            //push the query to database
+            $stmt->execute();
+            //set database connection and statement to null
+            $pdo = null;
+            $stmt = null;
+            header("Location: ../extra pages/registerSucess/registerSucess.html");
+            die();
+        } catch (PDOException $e) {
+             die("Quarry failed". $e ->getMessage());
+        }
+    } else {
+        header("location: reghome.html");
+    }
