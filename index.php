333656d058362200ef36909e83c5c74001eeef60
royboot <rgrizell467@gmail.com>
2024-01-23 20:15:04 +0100

    login
    added a login system

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
-
+<?php 
+    $login = require_once "login.inc.php";
+    session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -10,13 +13,13 @@
     <!--Main css file-->
     <link rel="stylesheet" href="style.css">
     <!--Css file to normalize all browsers settings across all browers-->
-    <link rel="stylesheet"href="node_modules/normalize.css/normalize.css">
+    <link rel="stylesheet" href="node_modules/normalize.css/normalize.css">
     <!--Favcon-->
     <link rel="icon" href="images/favcon.png">
     <!--Main js file-->
     <script src="index.js"></script>
     <!--Extra js file-->
-    <script src = "includes/index.js"></script>
+    <script src="includes/index.js"></script>
 </head>
 
 <body>
@@ -25,10 +28,12 @@
             <ul class="nav">
                 <li><a href="#main" class="nav-link nav-link-ltr">Main</a></li>
                 <li><a class="nav-link nav-link-ltr">Dashboard</a></li>
-                <li><a href = "includes/extra pages/Commands/commands.html" class="nav-link nav-link-ltr">Commands</a></li>
+                <li><a href="includes/extra pages/Commands/commands.html" class="nav-link nav-link-ltr">Commands</a>
+                </li>
                 <li><a href="https://github.com/Roy123132123/Starbot" class="nav-link nav-link-ltr">Github</a></li>
                 <!--Lgmb = Login main button-->
-                <li><button class="nav-link nav-link-ltr flt-right Lgmb fw"onclick="document.getElementById('m1').style.display = 'block'">Login</button></li>
+                <li><button class="nav-link nav-link-ltr flt-right Lgmb fw"
+                        onclick="document.getElementById('m1').style.display = 'block'">Login</button></li>
             </ul>
         </div>
     </header>
@@ -41,49 +46,52 @@
         </div>
     </main>
     <div class="flex">
-    <div class="Modal" id="m1">
-        <form class="modal-content animate"  method="post">
-            <div class="modal-imgcontainer">
-                <span onclick="document.getElementById('m1').style.display = 'none'" class="close"
-                    title="Close modal"></span>
-                <img src="images/user.png" alt="Avatar" class="avatar">
-            </div>
-            <div class="modal-container margin-top">
-                <!--Username-->
-                <label for="Uname"><b>Username</b></label>
-                <input type="text" placeholder="Enter Username" name="uname" id="Uname" required>
-                <!--Email-->
-                <label for="email"><b>Email</b></label>
-                <input type="text" placeholder="Enter email" name="email" id="email" required>
-                <!--Password-->
-                <label for="psw"><b>Password</b></label>
-                <input type="password" placeholder="Enter password" name="psw" id="psw" required>
-                <!--Login buton-->
-                <button type="submit" class="Login fw">Login</button>
-                <!--Remember me button-->
-                <label>
-                    <input class = "Rmb" type="checkbox" checked="checked" name="remember">Remember me
-                </label>
-            </div>
-            <div class="modal-container" style="background-color: #f1f1f1">
-                <button type="button" onclick="document.getElementById('m1').style.display = 'none'"
-                    class="cancelbtn fw">Cancel</button>
-                <div class = "flex-inline">
-                <span class="reg"><a href = 'includes/register_page/reghome.php' class = "reg fw">Register</a></span>
-                <!--<span class="psw fw"><a>Forgot password?</a></span>-->
-                <button id="Psw" class="psw fw" type="button" method = "post" onclick="Head();">Forgot Password</button>
-                <script>var PswButton = document.getElementById('Psw');
-                        function Head (){
-                            window.location.href = 'http://localhost/Starbot/includes/register_page/reghome.php';
-                        }
-                </script>
+        <div class="Modal" id="m1">
+            <form class="modal-content animate" method="post">
+                <div class="modal-imgcontainer">
+                    <span onclick="document.getElementById('m1').style.display = 'none'" class="close"
+                        title="Close modal"></span>
+                    <img src="images/user.png" alt="Avatar" class="avatar">
                 </div>
-            </div>
-        </form>
+                <div class="modal-container margin-top">
+                    <!--Email-->
+                    <label for="email"><b>Email</b></label>
+                    <input type="text" placeholder="Enter email" name="email" id="email" required>
+                    <!--Password-->
+                    <label for="psw"><b>Password</b></label>
+                    <input type="password" placeholder="Enter password" name="psw" id="psw" required>
+                    <!--Login buton-->
+                    <button type="submit" class="Login fw">Login</button>
+                    <?php
+                    if ($is_invalid): ?>
+                        <em>Invalid Login</em>
+                    <?php endif; ?>
+                    <!--Remember me button-->
+                    <label>
+                        <input class="Rmb" type="checkbox" checked="checked" name="remember">Remember me
+                    </label>
+                </div>
+                <div class="modal-container" style="background-color: #f1f1f1">
+                    <button type="button" onclick="document.getElementById('m1').style.display = 'none'"
+                        class="cancelbtn fw">Cancel</button>
+                    <div class="flex-inline">
+                        <span class="reg"><a href='includes/register_page/reghome.php'
+                                class="reg fw">Register</a></span>
+                        <!--<span class="psw fw"><a>Forgot password?</a></span>-->
+                        <button id="Psw" class="psw fw" type="button" method="post" onclick="Head();">Forgot
+                            Password</button>
+                        <script>var PswButton = document.getElementById('Psw');
+                            function Head() {
+                                window.location.href = 'http://localhost/Starbot/includes/register_page/reghome.php';
+                            }
+                        </script>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
     <footer class="ftr">
-        <span><a class="DiscTxt"><img src="images/discord.png" alt="discord icon"class="DiscImg"></a></span>
+        <span><a class="DiscTxt"><img src="images/discord.png" alt="discord icon" class="DiscImg"></a></span>
     </footer>
 </body>
 
---
+++ login.inc.php
@@ -0,0 +1,25 @@
+<?php
+$is_invalid = false;
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST["email"];
+    $password = $_POST["psw"];
+    $dbcon = require_once "includes/dbh.inc.php";
+
+ try {
+    $stmt = $pdo->prepare("SELECT* FROM users WHERE email =:email");
+    $stmt->bindParam(":email", $email);
+    $stmt->execute();
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+    if ($user && password_verify($password, $user["password_hash"])) {
+        $_SESSION["email"] = $email;
+        header("location:index.php");
+        exit();
+    }else{
+        $is_invalid = true;
+    }
+ } catch (PDOException $e) {
+    die("connection failed". $e->getMessage());
+ }
+} else {
+
+}
