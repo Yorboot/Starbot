f3ac43df8945ce87fc1f396d7a03286e9096d560
Royboot <rgrizell467@gmail.com>
2024-06-03 13:42:42 +0200

    Register page
    Register page changes

--- includes/register_page/reghome.php
+++ includes/register_page/reghome.php
@@ -1,36 +1,40 @@
-
-
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <meta charset='utf-8'>
-    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title>Register</title>
-    <link rel="icon" type="image/png" href = "../../images/favcon.png">
-    <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel='stylesheet' type='text/css' media='screen' href='register.css'>
-
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="icon" href="../../images/favcon.png">
+    <link rel="stylesheet" href="../../node_modules/normalize.css/normalize.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
-    <div class = "mcontainer">
-    <form action = "signup.inc.php" method='POST' novalidate>
-        <!--Title-->
-        <h1 class = "Title">Register</h1>
-        <div class = "inputs">
-        <!--Email-->
-        <label for="Email"><b>Email</b></label>
-        <input type ="text" placeholder="Enter Email" name = "email" id  = "Email"required>
-        <!--Passwoord-->
-        <label for="psw" class = "psw"><b>Password</b></label>
-        <input type="password" placeholder="Enter Password" name="psw" required>
-        <!--Repeat password-->
-        <label for="Rpsw" class = "psw"><b>Repeat Password</b></label>
-        <input type="password" placeholder="Enter Password"  name="Rpsw" required>
-        </div>
-        <!--Submit button-->
-        <button type="submit" class = "reg">Register</button>
-    </div>
-    </form>
+    <main class="Container">
+        <form method="POST" class = "Modal">
+            
+            <div class ="Flex FD">
+                <h1 class="Title">Welcome back!</h1>
+                <input class="Inputs" type="text" placeholder="Enter email">
+                <input class="Inputs" type="password" placeholder="Enter Password">
+                <input class="Inputs"type = "password" placeholder="Confirm Password">
+                <button type="submit" class="Submit">Register</button>
+            </div>
+            <div class="Flex">
+                <div class="Line"></div>
+                <div class="OlwT">Or Register With</div>
+                <div class="Line"></div>
+            </div>
+            <div>
+                 <ul class="Boxes FlexB">
+                    <li class=""><a href=""><img src="../../images/facebook.png" alt="" class="facebook"></a></li>
+                    <li class="Mb"><a href=""><img src="../../images/search.png" alt="" class="Google"></a></li>
+                    <li class=""><a href=""><img src="../../images/user.png" alt="" class="Img"></a></li>
+                </ul>
+            </div>
+            <div class="Flex FgtPsw">
+                <span>Already have an acount?<a href="../extra-pages/Login/index.html">Login</a></span>
+            </div>
+        </form>
+    </main>
 </body>
 </html>
--- index.php
+++ index.php
@@ -31,8 +31,7 @@
                 </li>
                 <li><a href="https://github.com/Roy123132123/Starbot" class="nav-link nav-link-ltr">Github</a></li>
                 <!--Lgmb = Login main button-->
-                <li><button class="nav-link nav-link-ltr flt-right Lgmb fw"
-                        onclick="document.getElementById('m1').style.display = 'block'">Login</button></li>
+                <li><a class= "nav-link nav-link-ltr flt-right Lgmb fw" href="includes/extra-pages/Login/index.html">Login</a></li>
             </ul>
         </div>
     </header>
@@ -44,50 +43,7 @@
             </div>
         </div>
     </main>
-    <div class="flex">
-        <div class="Modal" id="m1">
-            <form class="modal-content animate" method="post">
-                <div class="modal-imgcontainer">
-                    <span onclick="document.getElementById('m1').style.display = 'none'" class="close"
-                        title="Close modal"></span>
-                    <img src="images/user.png" alt="Avatar" class="avatar">
-                </div>
-                <div class="modal-container margin-top">
-                    <!--Email-->
-                    <label for="email"><b>Email</b></label>
-                    <input type="text" placeholder="Enter email" name="email" id="email" required>
-                    <!--Password-->
-                    <label for="psw"><b>Password</b></label>
-                    <input type="password" placeholder="Enter password" name="psw" id="psw" required>
-                    <!--Login buton-->
-                    <button type="submit" class="Login fw">Login</button>
-                    <?php
-                    if ($is_invalid): ?>
-                        <em>Invalid Login</em>
-                    <?php endif; ?>
-                    <!--Remember me button-->
-                    <label>
-                        <input class="Rmb" type="checkbox" checked="checked" name="remember">Remember me
-                    </label>
-                </div>
-                <div class="modal-container" style="background-color: #f1f1f1">
-                    <button type="button" onclick="document.getElementById('m1').style.display = 'none'"
-                        class="cancelbtn fw">Cancel</button>
-                    <div class="flex-inline">
-                        <span class="reg"><a href='includes/register_page/reghome.php'
-                                class="reg fw">Register</a></span>
-                        <!--<span class="psw fw"><a>Forgot password?</a></span>-->
-                        <button id="Psw" class="psw fw" type="button" method="post" onclick="Head();">Forgot
-                            Password</button>
-                        <script>var PswButton = document.getElementById('Psw');
-                            function Head() {
-                                window.location.href = 'http://localhost/Starbot/includes/register_page/reghome.php';
-                            }
-                        </script>
-                    </div>
-                </div>
-            </form>
-        </div>
+
     </div>
     <footer class="ftr">
         <span><a class="DiscTxt"><img src="images/discord.png" alt="discord icon" class="DiscImg"></a></span>
